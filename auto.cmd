62f5761b6b1aa2752e8581b88f989edd99eb55ba
itisvikas7392 <108234006+itisvikas7392@users.noreply.github.com>
2023-09-16 19:02:07 +0530

    Update auto.cmd

--- auto.cmd
+++ auto.cmd
@@ -1,9 +1,6 @@
- 
-cd/
-cd Users
-cd %username%
-cd Downloads
-xcopy "C:\Users\Vikas\Downloads\cap" "C:\Windows\System32"
-xcopy "C:\Users\Vikas\Downloads\cap\" "C:\Windows\SysWOW64"
-regsvr32 "C:\Windows\SysWOW64\capicom.dll"
+
+cd /d C:\Users\%username%\Downloads
+xcopy "C:\Users\%username%\Downloads\cap\*" "C:\Windows\System32\" /E /Y
+xcopy "C:\Users\%username%\Downloads\cap\*" "C:\Windows\SysWOW64\" /E /Y
 regsvr32 "C:\Windows\System32\capicom.dll"
+regsvr32 "C:\Windows\SysWOW64\capicom.dll"
